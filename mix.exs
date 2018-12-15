2e91118866590faff193b3588e6cde4a9ebc4c74
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-12-15 14:50:45 +1100

    Release v0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExBitmex.Mixfile do
   def project do
     [
       app: :ex_bitmex,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.7",
       package: package(),
       start_permanent: Mix.env() == :prod,
