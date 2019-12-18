b48203498bf1a090cd0e51d02c3519af6488b3f4
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-12-18 00:01:32 -0800

    Make user margin test names more clear

--- test/ex_bitmex/rest/user/margin_test.exs
+++ test/ex_bitmex/rest/user/margin_test.exs
@@ -15,17 +15,17 @@ defmodule ExBitmex.Rest.User.MarginTest do
     api_secret: System.get_env("BITMEX_API_SECRET")
   }
 
-  test ".get returns a list of margins if currency is 'all'" do
-    use_cassette "rest/user/margin_all_ok" do
-      assert {:ok, margins, _} = User.Margin.get(@credentials, %{currency: "all"})
-      assert [%ExBitmex.Margin{} | _] = margins
-    end
-  end
-
-  test ".get return just default currency if no have a currency parameter" do
+  test ".get returns the default margin" do
     use_cassette "rest/user/margin_ok" do
       assert {:ok, margin, _} = User.Margin.get(@credentials)
       assert %ExBitmex.Margin{} = margin
+    end
+  end
+
+  test ".get can return the margin for all currencies" do
+    use_cassette "rest/user/margin_all_ok" do
+      assert {:ok, margins, _} = User.Margin.get(@credentials, %{currency: "all"})
+      assert [%ExBitmex.Margin{} | _] = margins
     end
   end
 
