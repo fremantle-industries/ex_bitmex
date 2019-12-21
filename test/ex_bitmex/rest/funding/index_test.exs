9f0b48ec4332f01d2548d6a4db594944d0bc4794
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-12-20 16:30:27 -0800

    Add bubble error test coverage to funding index

--- test/ex_bitmex/rest/funding/index_test.exs
+++ test/ex_bitmex/rest/funding/index_test.exs
@@ -1,6 +1,8 @@
 defmodule ExBitmex.Rest.Funding.IndexTest do
   use ExUnit.Case, async: false
   use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+  import Mock
+  alias ExBitmex.Rest.Funding
   doctest ExBitmex.Rest.Funding.Index
 
   setup_all do
@@ -10,11 +12,16 @@ defmodule ExBitmex.Rest.Funding.IndexTest do
 
   test ".get returns funding history" do
     use_cassette "rest/funding/index_ok" do
-      {:ok, fundings, _} =
-        ExBitmex.Rest.Funding.Index.get(%{symbol: "XBTUSD", start: 0, count: 50})
+      {:ok, fundings, _} = Funding.Index.get(%{symbol: "XBTUSD", start: 0, count: 50})
 
       assert Enum.count(fundings) == 50
       assert [%ExBitmex.Funding{} | _] = fundings
+    end
+  end
+
+  test ".get bubbles errors" do
+    with_mock HTTPoison, request: fn _url -> {:error, %HTTPoison.Error{reason: :timeout}} end do
+      assert {:error, :timeout, nil} = Funding.Index.get()
     end
   end
 end
