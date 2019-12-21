8c921bf45ad75bf8eb726f5d2b0719d49c8cf724
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-12-20 16:35:05 -0800

    Add bubble error test coverage to trade index

--- test/ex_bitmex/rest/trade/index_test.exs
+++ test/ex_bitmex/rest/trade/index_test.exs
@@ -1,6 +1,7 @@
 defmodule ExBitmex.Rest.Trade.IndexTest do
   use ExUnit.Case, async: false
   use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+  import Mock
   alias ExBitmex.Rest.Trade
   doctest ExBitmex.Rest.Trade.Index
 
@@ -9,11 +10,17 @@ defmodule ExBitmex.Rest.Trade.IndexTest do
     :ok
   end
 
-  test ".all returns an ok tuple with the trades" do
-    use_cassette "rest/trades/all_ok" do
+  test ".get returns an ok tuple with the trades" do
+    use_cassette "rest/trade/index_ok" do
       {:ok, trades, _} = Trade.Index.get(%{symbol: "XBTUSD", start: 0, count: 50})
       assert Enum.count(trades) == 50
       assert [%ExBitmex.Trade{} | _] = trades
+    end
+  end
+
+  test ".get bubbles errors" do
+    with_mock HTTPoison, request: fn _url -> {:error, %HTTPoison.Error{reason: :timeout}} end do
+      assert {:error, :timeout, nil} = Trade.Index.get()
     end
   end
 end
