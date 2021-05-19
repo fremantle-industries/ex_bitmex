1be0c7326fc225ee8af6cde58e87c976f1f1560f
Alex <alex+git@fremantle.io>
2021-05-19 15:47:09 -0700

    Add Erlang 24 support
    - Use :crypto.mac/4 instead of :crypto.hmac/3

--- lib/ex_bitmex/auth.ex
+++ lib/ex_bitmex/auth.ex
@@ -3,7 +3,10 @@ defmodule ExBitmex.Auth do
 
   def sign(api_secret, verb, encoded_path, nonce, data) do
     payload = verb <> encoded_path <> to_string(nonce) <> data
-    :sha256 |> :crypto.hmac(api_secret, payload) |> Base.encode16(case: :lower)
+
+    :hmac
+    |> :crypto.mac(:sha256, api_secret, payload)
+    |> Base.encode16(case: :lower)
   end
 
   def nonce, do: :os.system_time(:micro_seconds) - @nonce_shift
